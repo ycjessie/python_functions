8e867997af8453c0b07ca6864210ed388b6cf813
marcwright-rem <marc@generalassemb.ly>
2020-12-01 13:24:34 -0600

    add prompt to starter

--- starter.py
+++ starter.py
@@ -1,1 +1,19 @@
+# 1. Write a function named sum_to() that takes a number parameter n and returns the sum of the numbers from 1 to n. For example:
 
+
+
+
+
+# 2. Write a function named largest() that takes a list parameter and returns the largest element in that list. You can assume the list contents are all positive numbers. For example:
+
+
+
+
+
+# 3. Write a function named occurances() that takes two string parameters and counts the number of occurrances of the second string inside the first string. For example:
+
+
+
+
+
+# 4. Write a function named product() that takes an arbitrary number of parameters, multiplies them all together, and returns the product. (HINT: Review your notes on *args).
